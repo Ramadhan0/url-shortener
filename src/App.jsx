965d3620b5ee8ac8f29a8d3efb3962c088a57bf9
Ramadhan <ramadhan@Ramadhans-MacBook-Air.local>
2025-03-09 18:39:54 +0200

    set up project

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+export default function App() {
+  return (
+    <div className="text-3xl font-bold underline bg-slate-900 h-screen text-slate-100 flex justify-center items-center">
+      <div class="table w-5/6 no-underline">
+        <div class="table-header-group">
+          <div class="table-row">
+            <div class="table-cell text-left ... no-underline">Song</div>
+            <div class="table-cell text-left ...">Artist</div>
+            <div class="table-cell text-left ...">Year</div>
+          </div>
+        </div>
+        <div class="table-row-group">
+          <div class="table-row">
+            <div class="table-cell ...">The Sliding Mr. Bones (Next Stop, Pottersville)</div>
+            <div class="table-cell ...">Malcolm Lockyer</div>
+            <div class="table-cell ...">1961</div>
+          </div>
+          <div class="table-row">
+            <div class="table-cell ...">Witchy Woman</div>
+            <div class="table-cell ...">The Eagles</div>
+            <div class="table-cell ...">1972</div>
+          </div>
+          <div class="table-row">
+            <div class="table-cell ...">Shining Star</div>
+            <div class="table-cell ...">Earth, Wind, and Fire</div>
+            <div class="table-cell ...">1975</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
